97f476302aa9a29f031975a24867c1f7fcb9f49e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-02 04:55:53 +0100

    ucnv: Add ICS symbols for back-compat
    Blobs linked with ICS's libicuuc.so need these
    
    Change-Id: I946e4e4eabdd2d9dc8e94a3d5b747ea6c19ab0aa

--- common/ucnv.c
+++ common/ucnv.c
@@ -2906,7 +2906,78 @@ ucnv_isFixedWidth(UConverter *cnv, UErrorCode *status){
             return FALSE;
     }
 }
+
+U_CAPI void    U_EXPORT2 ucnv_setToUCallBack_46 (UConverter * converter,
+                            UConverterToUCallback newAction,
+                            const void* newContext,
+                            UConverterToUCallback *oldAction,
+                            const void** oldContext,
+                            UErrorCode * err) {
+       ucnv_setToUCallBack(converter, newAction, newContext,
+                               oldAction, oldContext, err);
+}
+
+U_CAPI void  U_EXPORT2 ucnv_close_46 (UConverter * converter) {
+       ucnv_close (converter);
+}
+
+U_CAPI void  U_EXPORT2
+ucnv_setFromUCallBack_46 (UConverter * converter,
+                            UConverterFromUCallback newAction,
+                            const void* newContext,
+                            UConverterFromUCallback *oldAction,
+                            const void** oldContext,
+                            UErrorCode * err) {
+
+       ucnv_setFromUCallBack(converter, newAction, newContext, oldAction,
+                               oldContext, err);
+}
+
+U_CAPI void U_EXPORT2
+ucnv_convertEx_46(UConverter *targetCnv, UConverter *sourceCnv,
+               char **target, const char *targetLimit,
+               const char **source, const char *sourceLimit,
+               UChar *pivotStart, UChar **pivotSource,
+               UChar **pivotTarget, const UChar *pivotLimit,
+               UBool reset, UBool flush,
+               UErrorCode *pErrorCode) {
+       ucnv_convertEx(targetCnv, sourceCnv, target, targetLimit, source,
+                               sourceLimit, pivotStart, pivotSource,
+                               pivotTarget, pivotLimit, reset, flush,
+                               pErrorCode);
+}
+
+U_CAPI UConverter* U_EXPORT2
+ucnv_open_46 (const char *name,
+                       UErrorCode * err) {
+       return ucnv_open(name, err);
+}
+
+U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_46 (
+                  const void *context,
+                  UConverterFromUnicodeArgs *fromUArgs,
+                  const UChar* codeUnits,
+                  int32_t length,
+                  UChar32 codePoint,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err) {
+       return UCNV_FROM_U_CALLBACK_STOP(context, fromUArgs, codeUnits,
+                       length, codePoint, reason, err);
+}
+
+U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_46 (
+                  const void *context,
+                  UConverterToUnicodeArgs *toUArgs,
+                  const char* codeUnits,
+                  int32_t length,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err) {
+       return UCNV_TO_U_CALLBACK_STOP(context, toUArgs, codeUnits, length,
+                       reason, err);
+}
+
 #endif
+
 
 /*
  * Hey, Emacs, please set the following:
